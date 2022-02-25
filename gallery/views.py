ff6e42b4f9b58c05a276e865f6d7906cbb7a8a9d
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-02-25 18:52:23 +0300

    add search image view

--- gallery/views.py
+++ gallery/views.py
@@ -12,3 +12,20 @@ def gallery(request):
     title = 'Galleria'
 
     return render(request, 'index.html', {'title':title, 'images':images, 'locations':locations})
+
+#search for image functionality
+def search_image(request):
+    title = 'Search'
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    if 'image_category' in request.GET and request.GET['image_category']:
+        search_term = request.GET.get('image_category')
+        found_results = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        print(search_term)
+        print(found_results)
+
+        return render(request, 'search.html',{'title':title,'images': found_results, 'message': message, 'categories': categories, "locations":locations})
+    else:
+        message = 'No searches yet'
+        return render(request, 'search.html',{"message": message})
