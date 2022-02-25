04ba80764cec029da5919f6dfe96b11e1f303b80
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-02-25 14:48:00 +0300

    add methods to image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,6 @@
 from unicodedata import category
 from django.db import models
+from httpx import delete
 from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
@@ -62,9 +63,43 @@ class Image(models.Model):
     image_location = models.ForeignKey('Location')
     image_category = models.ForeignKey('Category')
 
+    def save_image(self):
+        self.save()
 
+    def delete_image(self):
+        self.delete()
 
-    
+    @classmethod
+    def update_image(cls,id, image,image_name, Image_description, image_location, image_category):
+        update = cls.objects.filter(id = id).update( image_name=image_name, Image_description=Image_description, image_location=image_location, image_category=image_category)
+        return update
+ 
+
+    @classmethod
+    def get_all_images(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = cls.objects.filter(id= id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls,image_category):
+        images = Image.objects.filter(image_category__name__icontains=image_category)
+        return images
+
+    @classmethod
+    def filter_by_location(cls, image_location):
+        images_location = cls.objects.filter(image_location__id=image_location)
+        return images_location
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['image_name']    
 
 
 
