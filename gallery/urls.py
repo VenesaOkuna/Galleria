aba045bfe6abdb9d67479eef509bb0d4813e3e90
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-02-25 18:59:09 +0300

    add search functionality path

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.gallery, name = 'gallery')
+    path('', views.gallery, name = 'gallery'),
+    path('search/', views.search_image, name = 'search_image')
 ]
