b312986074a42154b0573ed3efcebd3d1be7c8d1
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-02-25 14:06:53 +0300

    add image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
+
+
+class Image(models.Model):
+    image = ImageField( blank = False, manual_crop= 60)
+    image_name = models.CharField(max_length = 60)
+    image_description = models.TextField()
+    image_location = models.ForeignKey('Location')
+    image_category = models.ForeignKey('Category')
+
+
+
+    
+
+
+
