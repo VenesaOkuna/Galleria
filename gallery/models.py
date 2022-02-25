fc3a293bb64adedc1ea03cfadc163b68cb8650e0
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-02-25 15:05:43 +0300

    register models in admin

--- gallery/models.py
+++ gallery/models.py
@@ -60,8 +60,8 @@ class Image(models.Model):
     image = ImageField( blank = False, manual_crop= 60)
     image_name = models.CharField(max_length = 60)
     image_description = models.TextField()
-    image_location = models.ForeignKey('Location')
-    image_category = models.ForeignKey('Category')
+    image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image_category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def save_image(self):
         self.save()
