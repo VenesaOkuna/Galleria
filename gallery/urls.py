7ca50b6f11895feaae8114d7a98eadc1d67139b8
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-02-25 19:31:35 +0300

    add singleimage template and path

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.gallery, name = 'gallery'),
-    path('search/', views.search_image, name = 'search_image')
+    path('search/', views.search_image, name = 'search_image'),
+    path('^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single,name = 'single')
 ]
