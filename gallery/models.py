9702466d64041d2d5a20594abd8e26848abb24af
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-02-26 10:31:05 +0300

    test add media from admin server.

--- gallery/models.py
+++ gallery/models.py
@@ -87,7 +87,7 @@ class Image(models.Model):
 
     @classmethod
     def search_by_category(cls,image_category):
-        images = Image.objects.filter(image_category__name__icontains=image_category)
+        images = Image.objects.filter(image_category__category_name__icontains=image_category)
         return images
 
     @classmethod
