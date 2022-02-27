08aa6593171ec9be50f6075aafd512b6a85fb059
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-02-27 20:05:12 +0300

    image detail modall set up

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.gallery, name = 'gallery'),
     path('search/', views.search_image, name = 'search_image'),
-    path('image/<str:pk>/', views.Picture.as_view(),name='image'),
+    path('image/<image_id>/', views.Picture.as_view(),name='image'),
     path('image/<category_name>/<image_id>/)',views.single,name = 'single'),
     path('location/<image_location>/)', views.location_filter, name='location_filter')
 ]
