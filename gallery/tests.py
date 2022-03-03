b73ab32bf036285d8af057e0068bdb22ce1d56e4
VenesaOkuna <venesa.okuna@student.moringaschool.com>
2022-03-03 10:56:45 +0300

    category model test

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Location
+from .models import Location, Category
 
 # Create your tests here.
 
@@ -28,3 +28,30 @@ class LocationTestCLass(TestCase):
         location.update_location('Ngara')
         location = Location.get_location_id(self.loc.id)
         self.assertTrue(location.name == 'Ngara')
+
+
+class CategoryTestClass(TestCase):
+    # Set up Method
+    def setUp(self):
+        self.cat = Category(name="fashion")
+        self.cat.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.cat, Category))
+
+    def test_save_method(self):
+        self.cat.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+    def test_delete_method(self):
+        self.cat.save_category()
+        self.cat.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
+
+    def test_update(self):
+        category = Category.get_category_id(self.cat.id)
+        category.update_category('Travel')
+        category = Category.get_category_id(self.cat.id)
+        self.assertTrue(category.name == 'Travel')
